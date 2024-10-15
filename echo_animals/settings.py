e12b415649092686eabd8a9c41488aedcc86a1fc
klsoundarya <k.soundarya7@gmail.com>
2024-10-15 19:20:44 +0100

    add database url and secret key in settings.py

--- echo_animals/settings.py
+++ echo_animals/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3ff1s)&iqyc24pppnh6ad*2vrw8=qy)3s13zbj3yz99(^6@)@+'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
