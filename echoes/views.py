9f60ff59cdb3d44a8e1067fd74da9bd98f3683de
klsoundarya <k.soundarya7@gmail.com>
2024-10-20 15:35:58 +0100

    create & add class summernote and also add filter to echolist

--- echoes/views.py
+++ echoes/views.py
@@ -3,6 +3,6 @@ from django.views import generic
 from .models import BlogPost
 
 class EchoList(generic.ListView):
-    queryset = BlogPost.objects.all()
+    queryset = BlogPost.objects.filter(status=1).order_by("-created_on")
     template_name = 'echoes/echo_list.html' 
     context_object_name = 'object_list'
