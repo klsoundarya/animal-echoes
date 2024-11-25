756065fab98ef5063d1d9341dba60d5d950bbd12
klsoundarya <k.soundarya7@gmail.com>
2024-11-25 21:40:43 +0000

    write test cases  for like, edit and delete views from echoes app views

--- echoes/test_views.py
+++ echoes/test_views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.test import TestCase
 from .forms import CommentForm
-from .models import BlogPost
+from .models import BlogPost, Comment
 
 
 class TestBlogViews(TestCase):
@@ -22,6 +22,11 @@ class TestBlogViews(TestCase):
             status=1
         )
         self.post.save()
+        self.comment = Comment.objects.create(
+            post=self.post,
+            author=self.user,
+            body="Initial test comment"
+        )
 
     def test_render_post_detail_page_with_comment_form(self):
         response = self.client.get(reverse('post_detail', args=['echoes-title']))
@@ -55,3 +60,53 @@ class TestBlogViews(TestCase):
             b'Comment submitted and awaiting approval',
             response.content
         )
+
+    def test_like_view_toggle(self):
+        """Test toggling like/unlike functionality."""
+        self.client.login(username="testUsername", password="myPassword123")
+        url = reverse('like_post', args=[self.post.pk])
+
+        # Like the post
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(self.post.likes.filter(id=self.user.id).exists())
+
+        # Unlike the post
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(self.post.likes.filter(id=self.user.id).exists())
+
+    def test_comment_edit_success(self):
+        """Test editing a comment."""
+        self.client.login(username="testUsername", password="myPassword123")
+        edit_url = reverse('comment_edit', args=[self.post.slug, self.comment.pk])
+        post_data = {'body': 'Updated test comment'}
+
+        response = self.client.post(edit_url, post_data)
+        self.assertEqual(response.status_code, 302)
+        self.comment.refresh_from_db()
+        self.assertEqual(self.comment.body, 'Updated test comment')
+        self.assertFalse(self.comment.approved)
+
+    def test_comment_delete_success(self):
+        """Test deleting a comment."""
+        self.client.login(username="testUsername", password="myPassword123")
+        delete_url = reverse('comment_delete', args=[self.post.slug, self.comment.pk])
+
+        response = self.client.post(delete_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Comment.objects.filter(pk=self.comment.pk).exists())
+
+    def test_submit_blog_post(self):
+        """Test submitting a blog post."""
+        self.client.login(username="testUsername", password="myPassword123")
+        url = reverse('submit_blog_post')
+        post_data = {
+            'title': 'New Test Blog Post',
+            'content': 'This is a new blog post.',
+            'excerpt': 'A new blog post excerpt.'
+        }
+
+        response = self.client.post(url, post_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(BlogPost.objects.filter(title="New Test Blog Post").exists())
